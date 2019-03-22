e6bce0877cb8c8a2c497480f1d7cc1f36ac8d6be
JeanMahmoudDavid <nathan.moresco@icloud.com>
2019-03-22 16:20:34 +0100

    javascript

--- SQL/bddTPweb9.sql
+++ SQL/bddTPweb9.sql
@@ -14,30 +14,40 @@ CREATE TABLE Oeuvres (
 
 CREATE TABLE Theme (
   idTheme int(11) NOT NULL AUTO_INCREMENT,
-  Theme VARCHAR(64),
+  valtheme VARCHAR(64),
   PRIMARY KEY (idTheme)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE Annee (
   idAnnee int(11) NOT NULL AUTO_INCREMENT,
-  Annee DATE,
+  valannee int(11),
   PRIMARY KEY (idAnnee)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE Oeuvres
-ADD CONSTRAINT Annee_idAnnee
-FOREIGN KEY Oeuvres_idAnnee
-REFERENCES Oeuvres(idAnnee)
+ADD CONSTRAINT Oeuvres_idAnnee
+FOREIGN KEY (idAnnee)
+REFERENCES Annee (idAnnee);
 
-ALTER TABLE Theme
+ALTER TABLE Oeuvres
 ADD CONSTRAINT Oeuvres_idTheme
-FOREIGN KEY Oeuvres_idAnnee
-REFERENCES Theme(idOeuvres)
-
-FOREIGN KEY 
+FOREIGN KEY (idTheme)
+REFERENCES Theme (idTheme);
 
 INSERT INTO Oeuvres(Titre, Prix, Datecreat) VALUES
 ("Grenouille foireuse",2.0,"2018-06-24"),
 ("Dragon enflammé",223.5,"2016-06-24"),
 ("Souris fertile",0.7,"2017-06-24"),
 ("Gourde Quechua",7.0,"2019-06-24");
+
+INSERT INTO Annee(valannee) VALUES
+("2018"),
+("2019"),
+("2020"),
+("2021");
+
+INSERT INTO Theme(valtheme) VALUES
+("Animaux"),
+("Fantastique"),
+("Abstrait"),
+("Figurine");
