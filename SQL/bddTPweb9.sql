a604fa06dd60856766ee46f3cfc802f232daafc2
Yokunaa <hugodrp@icloud.com>
2019-03-22 15:39:28 +0100

    sql

--- SQL/bddTPweb9.sql
+++ SQL/bddTPweb9.sql
@@ -7,8 +7,34 @@ CREATE TABLE Oeuvres (
   Titre VARCHAR(64),
   Prix FLOAT,
   Datecreat DATE,
+  idTheme int(11),
+  idAnnee int(11),
 	PRIMARY KEY (idOeuvre)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Theme (
+  idTheme int(11) NOT NULL AUTO_INCREMENT,
+  Theme VARCHAR(64),
+  PRIMARY KEY (idTheme)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Annee (
+  idAnnee int(11) NOT NULL AUTO_INCREMENT,
+  Annee DATE,
+  PRIMARY KEY (idAnnee)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE Oeuvres
+ADD CONSTRAINT Annee_idAnnee
+FOREIGN KEY Oeuvres_idAnnee
+REFERENCES Oeuvres(idAnnee)
+
+ALTER TABLE Theme
+ADD CONSTRAINT Oeuvres_idTheme
+FOREIGN KEY Oeuvres_idAnnee
+REFERENCES Theme(idOeuvres)
+
+FOREIGN KEY 
 
 INSERT INTO Oeuvres(Titre, Prix, Datecreat) VALUES
 ("Grenouille foireuse",2.0,"2018-06-24"),
